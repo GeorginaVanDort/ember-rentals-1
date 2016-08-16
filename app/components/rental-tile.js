3d40166d79ecc8e040574a9a4072316d557c701c
Georgina Van Dort and Stewart Cole <georgina+stewart@>
2016-08-15 17:09:26 -0700

    creates all files

---
+++ app/components/rental-tile.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow() {
+      this.set('isImageShowing', true);
+    },
+    imageHide() {
+      this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
+    }
+  }
+});
